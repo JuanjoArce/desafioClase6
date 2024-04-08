0644510535b8dafab5c7e8a500ee771ace8493e9
Juanjo <juanjosearceherrera@gmail.com>
2024-04-08 19:53:37 -0300

    db

--- 1desafioArce-master/src/app.js
+++ 1desafioArce-master/src/app.js
@@ -1,22 +1,50 @@
 import express from 'express';
 import productRouter from "./routes/products.router.js"
 import usuariosRouter from "./routes/users.router.js"
+import handlerbars from 'express-handlebars';
+import __dirname from './utils.js';
+import { Server } from 'socket.io';
+
 
 const app = express();
+const PORT = 9090;
+
+
+//inicializamos el motor indicando con app.engine('que motor utilizaremos', el motor instanciado)
+app.engine('handlebars', handlerbars.engine());
+
+//Luego con app.set('views', ruta) indicamos en que parte del proyecto estaran las vistas , recuerda utilizar rutas absolutas pata evitar asuntos de ruteo relativo.
+app.set('views',__dirname+'/views');
+//Finalmente con app.set('view engine','handlebars') indicamos que el motor que ya inicializamos arriba es el que queremos utilizar. Es importante para saber que cuando digamos al servidor que renderice sepa que tiene que hacerlo con el motor de handlebars.
+app.set('views engine','handlebars');
+//seteamos de manera estatica nuestra carpeta public
+app.use(express.static(__dirname+'/public'))
+
+// app.get("/", (req, res)=>{
+//     let testUser = {
+//      name:"hilda",
+//      last_name:"martinez"
+//     }
+//     res.render('index',testUser);
+//  })
 
 //Preparar la configuracion del servidor para recibir objetos JSON.
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// //Routers
+// // //Routers
 app.use("/api/product", productRouter);
 app.use("/api/user", usuariosRouter);
 
-app.get("/", (req, res)=>{
-   res.send("Hola mundo!");
-});
 
-const SERVER_PORT = 9090;
-app.listen(SERVER_PORT, () =>{
-    console.log("Servidor escuchando por el puerto: " + SERVER_PORT);
+
+
+
+
+const httpServer = app.listen(PORT, () =>{
+    console.log(`Servidor escuchando por el puerto: ${PORT}`);
 });
+const socketServer = new Server(httpServer);
+socketServer.on('connection',socket => {
+    console.log(socket)
+})
--- 1desafioArce-master/src/routes/carts.router.js
+++ 1desafioArce-master/src/routes/carts.router.js
@@ -1,42 +1,42 @@
-import { Router } from 'express';
-import Cart from '../models/cart.model.js';
+// import { Router } from 'express';
+// import Cart from '../models/cart.model.js';
 
-const cartsRouter = Router();
+// const cartsRouter = Router();
 
-// POST para agregar un producto al carrito
-cartsRouter.post('/:cid/products', async (req, res) => {
-    try {
-        const { cid } = req.params;
-        const cart = await Cart.findById(cid);
-        if (!cart) {
-            return res.status(404).json({ status: 'error', message: 'Cart not found' });
-        }
+// // POST para agregar un producto al carrito
+// cartsRouter.post('/:cid/products', async (req, res) => {
+//     try {
+//         const { cid } = req.params;
+//         const cart = await Cart.findById(cid);
+//         if (!cart) {
+//             return res.status(404).json({ status: 'error', message: 'Cart not found' });
+//         }
 
-        // Agregar el nuevo producto al arreglo de productos del carrito
-        const { productId, quantity } = req.body;
-        cart.products.push({ productId, quantity });
-        await cart.save();
-        res.json({ status: 'success', message: 'Product added to cart' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ status: 'error', message: 'Internal Server Error' });
-    }
-});
+//         // Agregar el nuevo producto al arreglo de productos del carrito
+//         const { productId, quantity } = req.body;
+//         cart.products.push({ productId, quantity });
+//         await cart.save();
+//         res.json({ status: 'success', message: 'Product added to cart' });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+//     }
+// });
 
-// GET para obtener el contenido completo del carrito
-cartsRouter.get('/:cid', async (req, res) => {
-    try {
-        const { cid } = req.params;
-        const cart = await Cart.findById(cid);
-        if (!cart) {
-            return res.status(404).json({ status: 'error', message: 'Cart not found' });
-        }
+// // GET para obtener el contenido completo del carrito
+// cartsRouter.get('/:cid', async (req, res) => {
+//     try {
+//         const { cid } = req.params;
+//         const cart = await Cart.findById(cid);
+//         if (!cart) {
+//             return res.status(404).json({ status: 'error', message: 'Cart not found' });
+//         }
 
-        res.json({ status: 'success', payload: cart });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ status: 'error', message: 'Internal Server Error' });
-    }
-});
+//         res.json({ status: 'success', payload: cart });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+//     }
+// });
 
-export { cartsRouter };
+// export { cartsRouter };
